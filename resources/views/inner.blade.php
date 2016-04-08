20420963a7bf9ba0966b0f749587a042934c6686
Daniel Heyman <daniel.heyman@gmail.com>
2016-04-08 01:02:20 -0400

    Footer flex.

--- resources/views/inner.blade.php
+++ resources/views/inner.blade.php
@@ -24,15 +24,6 @@
 
     <body id="app" :class="{'bg': !loading}">
         
-        <div style="right: 5px; position: absolute; z-index: 10; top: 38px;">
-            @foreach(['health', 'coins/gold', 'coins/silver'] as $i)
-                <div style="position: relative; margin-top: 10px; background: rgba(0,0,0,.5); border-radius: 5px; padding: 5px; color: #fff; padding-right: 30px; "> 
-                    <span style=" line-height: 20px; ">10</span>
-                    <div style=" height: 20px; width: 20px; background: url(http://surf.local:8000/img/surf/icons/{{ $i }}.png); background-size: cover; position: absolute; top: 4px; right: 5px; "></div> 
-                </div>
-            @endforeach
-        </div>
-        
         <div v-show="loading" class="loader-big">
             <div class="loader">
                 <div class="ball"></div>
@@ -40,6 +31,15 @@
             </div>
         </div>
         <div v-show="!loading" v-el:main class="wrapper small-footer hidden">
+            <div style="right: 5px; position: absolute; z-index: 3; top: 38px;">
+                @foreach(['health', 'coins/gold', 'coins/silver'] as $i)
+                    <div style="position: relative; margin-top: 10px; background: rgba(0,0,0,.5); border-radius: 5px; padding: 5px; color: #fff; padding-right: 30px; "> 
+                        <span style=" line-height: 20px; ">10</span>
+                        <div style=" height: 20px; width: 20px; background: url(http://surf.local:8000/img/surf/icons/{{ $i }}.png); background-size: cover; position: absolute; top: 4px; right: 5px; "></div> 
+                    </div>
+                @endforeach
+            </div>
+            
             <div class="top @{{ currentView }}-view">
                 <component :is="currentView"></component>
 
@@ -73,7 +73,7 @@
                                     <div class="in" style="width: 80%; background: #545834;">60/80 EXP</div>
                                 </div>                                
                             </div>
-                            <div style="margin-left: 5px; color: #896D46; text-align: center;">
+                            <div style="margin-left: 5px; color: #896D46; text-align: center; padding-left: 50px;">
                                 accuracy (10) defense (10) stength (20) luck (5) speed (8)
                             </div>
                             <!--<health></health>-->
@@ -87,7 +87,7 @@
             </div>
 
             <span v-for="profile in openProfiles"><profile :user-id.sync="profile.id" :user-name.sync="profile.name" :on-close.sync="closeProfile"></profile></span>
-            <equip></equip>
+            <!--<equip></equip>-->
         </div>
     </body>
 
